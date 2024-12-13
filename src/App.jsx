6103a2602c31f82e45f4738b09dd5d7b5d7a0fdb
vaidehi2206 <vaidehivikramsinh.dabhi01@student.csulb.edu>
2024-12-13 10:58:39 -0800

    feat: updated package-lock.json, package.json, and src/index.css with new dependencies and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
+import Hero from './components/Hero'
 
 const App = () => {
   return (
-    <main>
-      <h1 className='text-5xl  text-violet-200 font-bold font-robert-medium'>
-        Welcome to Zephyer
-      </h1>
+    <main className='relative min-h-screen w-screen overflow-x-hidden'>
+      <Hero />
+
+      <section className='z-0 min-h-screen bg-blue-500'/>
+
     </main>
-  )
+  );
 }
 
 export default App
