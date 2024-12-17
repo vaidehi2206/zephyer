6743cb2a5a712b87fe2dad70182e33c0da6e85b6
vaidehi2206 <vaidehivikramsinh.dabhi01@student.csulb.edu>
2024-12-16 16:58:05 -0800

    Added AnimatedTitle component and updated About component to use it

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,9 @@
-import { useGSAP } from '@gsap/react'
-import gsap from 'gsap'
+import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/all'
+import AnimatedTitle from './AnimatedTitle'
+import { useGSAP } from '@gsap/react'
 
-gsap.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(ScrollTrigger);
 
 const About = () => {
     useGSAP(() => {
@@ -29,9 +30,10 @@ const About = () => {
             <h2 className='font-general text-sm uppercase md:text-[10px]'>
                 Welcome to Zephyer
             </h2>
-            <div className='mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]'>
-                Disc<b>o</b>ver the world's <br /> l<b>a</b>rgest shared adventure 
-            </div>
+
+            <AnimatedTitle 
+                title='Disc<b>o</b>ver the worlds <br /> l<b>a</b>rgest shared adventure' contianerClass='mt-5 !text-black text-center'
+            />
 
             <div className='about-subtext'>
                 <p>
